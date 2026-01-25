03b04877c4bd6153064ae4c85d2f362f24d8eadf
Marc Pfister <marcpfister1399@gmail.com>
2026-01-25 10:45:29 +0100

    Add proto mapping for the TOMMessage

--- proto/src/main/java/bftsmart/serialization/proto/ProtoMessageMapper.java
+++ proto/src/main/java/bftsmart/serialization/proto/ProtoMessageMapper.java
@@ -3,44 +3,89 @@ package bftsmart.serialization.proto;
 import bftsmart.communication.SystemMessage;
 import bftsmart.consensus.messages.ConsensusMessage;
 import bftsmart.consensus.messages.MessageFactory;
+import bftsmart.serialization.messages.TOMMessagePlain;
+import bftsmart.tom.core.messages.TOMMessageType;
+import com.google.protobuf.ByteString;
 
 public class ProtoMessageMapper {
-  static SystemMessage toInternal(ProtoMessages.SystemMessage msg) {
-    int senderId = msg.getSenderId();
+    static SystemMessage toInternal(ProtoMessages.SystemMessage msg) {
+        int senderId = msg.getSenderId();
 
-    switch (msg.getPayloadCase()) {
-      case CONSENSUS_MSG:
-        return toConsensusMessage(senderId, msg.getConsensusMsg());
-      case TOM_MSG:
-        break;
-      case PAYLOAD_NOT_SET:
-        break;
-      default:
-        break;
+        switch (msg.getPayloadCase()) {
+            case CONSENSUS_MSG:
+                return toConsensusMessage(senderId, msg.getConsensusMsg());
+            case TOM_MSG:
+                return toPlainTOMMessage(senderId, msg.getTomMsg());
+            case PAYLOAD_NOT_SET:
+                break;
+            default:
+                break;
+        }
+        return null;
     }
-    return null;
-  }
 
-  static ProtoMessages.SystemMessage fromInternal(SystemMessage msg) {
-    return null;
-  }
-
-  private static ConsensusMessage toConsensusMessage(int senderId, ProtoMessages.ConsensusMessage msg) {
-    MessageFactory factory = new MessageFactory(senderId);
-    switch (msg.getType()) {
-      case PROPOSE:
-        return factory.createPropose(msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
-      case WRITE:
-        return factory.createWrite(msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
-        case ACCEPT:
-        return factory.createAccept(msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
-      case REQ_DECISION:
-        return factory.createRequestDecision(msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
-      case FWD_DECISION:
-        return factory.createForwardDecision(msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
-      default:
-        break;
+    static ProtoMessages.SystemMessage fromInternal(SystemMessage msg) {
+        ProtoMessages.SystemMessage.Builder builder =
+                ProtoMessages.SystemMessage.newBuilder().setSenderId(msg.getSender());
+        if (msg instanceof TOMMessagePlain) {
+            builder.setTomMsg(fromPlainTOMMessage((TOMMessagePlain) msg));
+        } else {
+            return null;
+        }
+        return builder.build();
     }
-    return null;
-  }
+
+    private static ConsensusMessage toConsensusMessage(
+            int senderId, ProtoMessages.ConsensusMessage msg) {
+        MessageFactory factory = new MessageFactory(senderId);
+        switch (msg.getType()) {
+            case PROPOSE:
+                return factory.createPropose(
+                        msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
+            case WRITE:
+                return factory.createWrite(
+                        msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
+            case ACCEPT:
+                return factory.createAccept(
+                        msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
+            case REQ_DECISION:
+                return factory.createRequestDecision(
+                        msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
+            case FWD_DECISION:
+                return factory.createForwardDecision(
+                        msg.getNumber(), msg.getEpoch(), msg.getValue().toByteArray());
+            default:
+                break;
+        }
+        return null;
+    }
+
+    private static TOMMessagePlain toPlainTOMMessage(int senderId, ProtoMessages.TOMMessage msg) {
+        TOMMessagePlain plain = new TOMMessagePlain(senderId);
+        plain.setViewID(msg.getViewId());
+        plain.setType(TOMMessageType.getMessageType(msg.getTypeValue()));
+        plain.setSession(msg.getSession());
+        plain.setSequence(msg.getSequence());
+        plain.setOperationId(msg.getOperationId());
+        if (msg.getContent().toByteArray().length > 0) {
+            plain.setContent(msg.getContent().toByteArray());
+        }
+        plain.setReplyServer(msg.getReplyServer());
+        return plain;
+    }
+
+    private static ProtoMessages.TOMMessage fromPlainTOMMessage(TOMMessagePlain msg) {
+        ProtoMessages.TOMMessage.Builder tomMessage =
+                ProtoMessages.TOMMessage.newBuilder()
+                        .setViewId(msg.getViewID())
+                        .setTypeValue(msg.getType().ordinal())
+                        .setSession(msg.getSession())
+                        .setSequence(msg.getSequence())
+                        .setOperationId(msg.getOperationId())
+                        .setReplyServer(msg.getReplyServer());
+        if (msg.getContent() != null) {
+            tomMessage.setContent(ByteString.copyFrom(msg.getContent()));
+        }
+        return tomMessage.build();
+    }
 }
--- proto/src/main/java/bftsmart/serialization/proto/ProtoSerializer.java
+++ proto/src/main/java/bftsmart/serialization/proto/ProtoSerializer.java
@@ -1,21 +1,30 @@
 package bftsmart.serialization.proto;
 
+import bftsmart.communication.SystemMessage;
+import bftsmart.serialization.MessageSerializer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import bftsmart.communication.SystemMessage;
-import bftsmart.serialization.MessageSerializer;
 
 public class ProtoSerializer implements MessageSerializer {
 
-  @Override
-  public void serialize(SystemMessage msg, OutputStream out) throws IOException {
-    ProtoMessageMapper.fromInternal(msg).writeDelimitedTo(out);
-  }
+    private static final ProtoSerializer instance = new ProtoSerializer();
 
-  @Override
-  public SystemMessage deserialize(InputStream in) throws IOException {
-    ProtoMessages.SystemMessage msg = ProtoMessages.SystemMessage.parseDelimitedFrom(in);
-    return ProtoMessageMapper.toInternal(msg);
-  }
+    public static ProtoSerializer getInstance() {
+        return instance;
+    }
+
+    @Override
+    public <T extends SystemMessage> void serialize(T msg, OutputStream out) throws IOException {
+        ProtoMessageMapper.fromInternal(msg).writeDelimitedTo(out);
+    }
+
+    @Override
+    public <T extends SystemMessage> T deserialize(InputStream in, Class<T> clazz)
+            throws IOException, ClassNotFoundException {
+        ProtoMessages.SystemMessage result = ProtoMessages.SystemMessage.parseDelimitedFrom(in);
+        return (T) ProtoMessageMapper.toInternal(result);
+    }
+
+    private ProtoSerializer() {}
 }
