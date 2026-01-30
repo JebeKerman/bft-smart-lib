0ba93c0c1363d1da0091a3a7354ef398b6897ec2
Marc Pfister <marc@steuerberater-pfister.de>
2026-01-30 15:46:02 +0100

    Add test-fixtures module

--- test-fixtures/src/main/java/bftsmart/messages/test/VMMessageArbitrary.java
+++ test-fixtures/src/main/java/bftsmart/messages/test/VMMessageArbitrary.java
@@ -1,33 +1,21 @@
-package bftsmart.serialization.proto;
+package bftsmart.messages.test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import bftsmart.communication.SystemMessage;
-import bftsmart.messages.test.VMMessageArbitrary;
 import bftsmart.reconfiguration.ReconfigureReply;
 import bftsmart.reconfiguration.VMMessage;
 import bftsmart.reconfiguration.views.View;
-import bftsmart.serialization.MessageSerializer;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ArbitrarySupplier;
 import net.jqwik.api.Combinators;
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
 import net.jqwik.api.Provide;
-import net.jqwik.api.ShrinkingMode;
 
-public class TestVMMessageSerializer {
-    private final MessageSerializer serializer = ProtoSerializer.getInstance();
+public final class VMMessageArbitrary implements ArbitrarySupplier<VMMessage> {
 
-    @Provide
-    Arbitrary<VMMessage> messages() {
+    @Override
+    public Arbitrary<VMMessage> get() {
         return Combinators.combine(Arbitraries.integers(), replies()).as(VMMessage::new);
     }
 
@@ -85,49 +73,5 @@ public class TestVMMessageSerializer {
                                                     }
                                                     return null;
                                                 }));
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testSerializeSimpleVMMessage() throws IOException, ClassNotFoundException {
-        View view =
-                new View(
-                        1,
-                        new int[] {1, 2, 3},
-                        1,
-                        new InetSocketAddress[] {
-                            new InetSocketAddress("localhost", 8080),
-                            new InetSocketAddress("localhost", 8081),
-                            new InetSocketAddress("localhost", 8082)
-                        });
-        ReconfigureReply reply = new ReconfigureReply(view, new String[] {"node1", "node2"}, 1, 2);
-        VMMessage expected = new VMMessage(1, reply);
-
-        byte[] bytes = toBytes(expected);
-        assertTrue(bytes.length > 0);
-
-        VMMessage actual = fromBytes(bytes, VMMessage.class);
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
-    }
-
-    @Property(tries = 50, shrinking = ShrinkingMode.OFF)
-    public void testSerializeRandomVMMessage(
-            @ForAll(supplier = VMMessageArbitrary.class) VMMessage expected)
-            throws IOException, ClassNotFoundException {
-        byte[] bytes = toBytes(expected);
-        assertTrue(bytes.length > 0);
-
-        VMMessage actual = fromBytes(bytes, VMMessage.class);
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
-    }
-
-    private byte[] toBytes(SystemMessage msg) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        serializer.serialize(msg, outputStream);
-        return outputStream.toByteArray();
-    }
-
-    private <T extends SystemMessage> T fromBytes(byte[] bytes, Class<T> clazz)
-            throws IOException, ClassNotFoundException {
-        return serializer.deserialize(new ByteArrayInputStream(bytes), clazz);
     }
 }
