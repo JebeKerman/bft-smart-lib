334b88495765eb745e2a84ae0614a9b5f7517ee8
Marc Pfister <marc@steuerberater-pfister.de>
2026-02-17 11:45:51 +0100

    Move SMMessage and StandardSMMessage to messages

--- src/main/java/bftsmart/statemanagement/SMMessage.java
+++ src/main/java/bftsmart/statemanagement/SMMessage.java
@@ -15,11 +15,6 @@ limitations under the License.
 */
 package bftsmart.statemanagement;
 
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
-
-import bftsmart.communication.SystemMessage;
 import bftsmart.reconfiguration.views.View;
 import bftsmart.tom.util.TOMUtil;
 
@@ -28,16 +23,9 @@ import bftsmart.tom.util.TOMUtil;
  * 
  * @author Joao Sousa
  */
-public abstract class SMMessage extends SystemMessage {
+public abstract class SMMessage extends SMMessageWire {
 
     private ApplicationState state; // State log
-    private View view;
-    private int cid; // Consensus ID up to which the sender needs to be updated
-    private int type; // Message type
-    private int regency; // Current regency
-    private int leader; // Current leader
-    public final boolean TRIGGER_SM_LOCALLY; // indicates that the replica should
-                                             // initiate the SM protocol locally
 
     /**
      * Constructs a SMMessage
@@ -48,22 +36,12 @@ public abstract class SMMessage extends SystemMessage {
      * @param state State log
      */
     protected SMMessage(int sender, int cid, int type, ApplicationState state, View view, int regency, int leader) {
-        super(sender);
+        super(sender, cid, type, state, view, regency, leader, type == TOMUtil.TRIGGER_SM_LOCALLY && sender == -1);
         this.state = state;
-        this.view = view;
-        this.cid = cid;
-        this.type = type;
-        this.sender = sender;
-        this.regency = regency;
-        this.leader = leader;
-
-        if (type == TOMUtil.TRIGGER_SM_LOCALLY && sender == -1) this.TRIGGER_SM_LOCALLY = true;
-        else this.TRIGGER_SM_LOCALLY  = false;
-
     }
 
     protected SMMessage() {
-        this.TRIGGER_SM_LOCALLY = false;
+        super();
     }
     /**
      * Retrieves the state log
@@ -71,69 +49,5 @@ public abstract class SMMessage extends SystemMessage {
      */
     public ApplicationState getState() {
         return state;
-    }
-    
-    /**
-     * Retrieves the state log
-     * @return The state Log
-     */
-    public View getView() {
-        return view;
-    }
-    
-    /**
-     * Retrieves the type of the message
-     * @return The type of the message
-     */
-    public int getType() {
-        return type;
-    }
-
-    /**
-     * Retrieves the consensus ID up to which the sender needs to be updated
-     * @return The consensus ID up to which the sender needs to be updated
-     */
-    public int getCID() {
-        return cid;
-    }
-
-    /**
-     * Retrieves the regency that the replica had when sending the state
-     * @return The regency that the replica had when sending the state
-     */
-    public int getRegency() {
-        return regency;
-    }
-    
-    /**
-     * Retrieves the leader that the replica had when sending the state
-     * @return The leader that the replica had when sending the state
-     */
-    public int getLeader() {
-        return leader;
-    }
-    
-    @Override
-    public void writeExternal(ObjectOutput out) throws IOException{
-        super.writeExternal(out);
-        out.writeInt(sender);
-        out.writeInt(cid);
-        out.writeInt(type);
-        out.writeInt(regency);
-        out.writeInt(leader);
-        out.writeObject(state);
-        out.writeObject(view);
-    }
-
-    @Override
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException{
-        super.readExternal(in);
-        sender = in.readInt();
-        cid = in.readInt();
-        type = in.readInt();
-        regency = in.readInt();
-        leader = in.readInt();
-        state = (ApplicationState) in.readObject();
-        view = (View) in.readObject();
     }
 }
