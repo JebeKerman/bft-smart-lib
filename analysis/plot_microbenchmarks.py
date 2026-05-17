7a4a51730f85ac9c9cfe8d64473003e41285856d
Marc Pfister <marcpfister1399@gmail.com>
2026-05-17 16:50:12 +0200

    Fix microbenchmark analysis tool

--- analysis/plot_microbenchmarks.py
+++ analysis/plot_microbenchmarks.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import List, TypedDict
 import re
 import matplotlib.pyplot as plt
+import csv
 
 
 class LogResult(TypedDict):
@@ -32,27 +33,42 @@ def load_log_files(log_dir: str) -> List[LogResult]:
 
 def filter_server_logs(log_files: List[Path]):
     logs = [{
-                "id": int(p.name.split('.')[0].removeprefix('server_200_p')),
-                "path": p,
-                "content": p.read_text(),
-                "data_blocks": process_server_log(p.read_text()),
-            } for p in log_files if p.name.startswith("server_")]
+        "id": int(p.name.split('.')[0].removeprefix('server_p')),
+        "path": p,
+        "content": p.read_text(),
+        "data_blocks": process_server_log(p.read_text()),
+    } for p in log_files if p.name.startswith("server_")]
     return sorted(logs, key=lambda x: x["id"])
 
 
 def process_server_log(log_content: str):
     blocks = log_content.split('--- Measurements')[1:]
-    blocks = ['--- Measurements' + b.split('Batch average size')[0] + 'Batch average size' for b in blocks]
+    blocks = [
+        '--- Measurements' + b
+        for b in log_content.split('--- Measurements')[1:]
+    ]
 
     data_blocks = []
 
     for block in blocks:
+        msg_sizes = re.findall(r"(\w+)\s+ByteCount\s*=\s*(\d+):(\d+)", block)
+        msg_sizes = [
+            {
+                "name": name,
+                "byte_count": int(byte_count),
+                "msg_count": int(msg_count),
+                "avg_size": int(byte_count) / int(msg_count),
+            }
+            for name, byte_count, msg_count in msg_sizes
+        ]
+
         data = {
             'ops': int(re.search(r"Measurements after (\d+) ops", block).group(1)),
             'throughput': float(re.search(r"Throughput = ([\d.E+-]+) operations/sec", block).group(1)),
             'throughput_max': float(re.search(r"Maximum observed: ([\d.E+-]+) ops/sec", block).group(1)),
             'total_latency': float(re.search(r"Total latency = ([\d.E+-]+)", block).group(1)),
             'pro_consensus_latency': float(re.search(r"Pos-consensus latency = ([\d.E+-]+)", block).group(1)),
+            'msg_sizes': msg_sizes,
         }
         data_blocks.append(data)
 
@@ -60,6 +76,7 @@ def process_server_log(log_content: str):
 
 
 def plot_throughput(results: List[LogResult], plot_dir: Path):
+    plot_dir.mkdir(parents=True, exist_ok=True)
     filename = f"{plot_dir}/throughput.png"
     print(f"Generating throughput plot {filename}")
     for result in results:
@@ -112,15 +129,60 @@ def plot_latency(results: List[LogResult], plot_dir: Path):
     plt.close()
 
 
-def main(log_dir: Path):
-    log_files = load_log_files(log_dir)
-    plot_throughput(log_files, "analysis/out")
-    plot_latency(log_files, "analysis/out")
+def plot_msg_sizes(results: List[LogResult], plot_dir: Path):
+    filename = f"{plot_dir}/msg_size.png"
+
+    print(f"Generating message size plot {filename}")
+    print(f"Generating message size plot {plot_dir}/message_stats.csv")
+
+    with open(f"{plot_dir}/message_stats.csv", "w", newline="") as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=[
+                "serializer",
+                "message_type",
+                "byte_count",
+                # "msg_count",
+                # "avg_size",
+            ],
+        )
+        writer.writeheader()
+
+        rows = []
+        for result in results:
+            print(result['server_logs'][0]['data_blocks'][-1]['msg_sizes'])
+            for message in result['server_logs'][0]['data_blocks'][-1]['msg_sizes']:
+                rows.append({
+                    "serializer": result['serializer'],
+                    "message_type": message['name'],
+                })
+
+        print(rows)
+
+        writer.writerows(rows)
+
+    return
+    plt.title("Latency")
+    plt.ylabel("total latency [us]")
+    plt.xlabel("ops")
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(filename)
+    plt.close()
+
+
+def main(log_dir: Path, log_id: str):
+    log_files = load_log_files(log_dir / log_id)
+    out_dir = Path("analysis/out/", log_id)
+    plot_throughput(log_files, out_dir)
+    plot_latency(log_files, out_dir)
+    plot_msg_sizes(log_files, out_dir)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Usage: python {sys.argv[0]} <log_dir>")
+    if len(sys.argv) != 3:
+        print(f"Usage: python {sys.argv[0]} <log_dir> <log_id>")
         sys.exit(1)
 
-    main(Path(sys.argv[1]))
+    main(Path(sys.argv[1]), sys.argv[2])
